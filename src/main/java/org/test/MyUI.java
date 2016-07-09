3564424f028ee81795bb08b163587d5d85936266
greg-ashby <ashbygreg@gmail.com>
2016-07-09 12:58:56 -0600

    added a test chart and custom layout

--- src/main/java/org/test/MyUI.java
+++ src/main/java/org/test/MyUI.java
@@ -2,16 +2,20 @@ package org.test;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.test.ChartsData.ShoeSizeInfo;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.DataSeries;
+import com.vaadin.addon.charts.model.DataSeriesItem;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -26,22 +30,31 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+    	CustomLayout layout = new CustomLayout("layout");
+    	setContent(layout);
+    	
+    	
+    	Chart chart = new Chart();
+		Configuration conf = chart.getConfiguration();
+		conf.setTitle("Hello Charts!");
+		layout.addComponent(chart, "chart");
+    	
+		conf.getChart().setType(ChartType.LINE);
+		ChartsData data = new ChartsData();
+		DataSeries girls = new DataSeries("Girls");
+		for (ShoeSizeInfo shoeSizeInfo : data.getGirlsData()) {
+			// Shoe size on the X-axis, age on the Y-axis
+			girls.add(new DataSeriesItem(shoeSizeInfo.getSize(), shoeSizeInfo.getAgeMonths() / 12.0f));
+		}
+		conf.addSeries(girls);
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
-        
-        setContent(layout);
+		DataSeries boys = new DataSeries("Boys");
+		for (ShoeSizeInfo shoeSizeInfo : data.getBoysData()) {
+			// Shoe size on the X-axis, age on the Y-axis
+			boys.add(new DataSeriesItem(shoeSizeInfo.getSize(), shoeSizeInfo.getAgeMonths() / 12.0f));
+		}
+		conf.addSeries(boys);
+  	
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
